b131fd81ac48f1abdce849d12471630ba17dc0c8
Jérôme Gangneux <jerome@gangneux.net>
2026-03-02 09:41:53 +0100

    update composer and fix interface bug

--- server/src/Entity/ResetPasswordRequest.php
+++ server/src/Entity/ResetPasswordRequest.php
@@ -21,17 +21,19 @@ class ResetPasswordRequest implements ResetPasswordRequestInterface
         }
     }
 
+    /** @noinspection PhpGetterAndSetterCanBeReplacedWithPropertyHooksInspection */
     #[ORM\ManyToOne]
     #[ORM\JoinColumn(nullable: false)]
-    public ?Admin $user = null {
-        get {
-            return $this->user;
-        }
-    }
+    private Admin $user;
 
     public function __construct(Admin $user, \DateTimeInterface $expiresAt, string $selector, string $hashedToken)
     {
         $this->user = $user;
         $this->initialize($expiresAt, $selector, $hashedToken);
+    }
+
+    public function getUser(): object
+    {
+        return $this->user;
     }
 }
